a2fc8ca0376cf276212f098dddc272841f396981
Vasanth <vasanth.fine@yahoo.com>
2024-04-04 20:58:07 +0300

    Changed annotation name.

--- src/main/java/com/country/details/democountry/controller/CountryController.java
+++ src/main/java/com/country/details/democountry/controller/CountryController.java
@@ -3,6 +3,8 @@ package com.country.details.democountry.controller;
 import com.country.details.democountry.modal.Country;
 import com.country.details.democountry.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import java.util.List;
  * *
  */
 @RestController
+@RefreshScope
 public class CountryController {
 
     @Autowired
@@ -21,7 +24,7 @@ public class CountryController {
      * *
      * @return
      */
-    @RequestMapping(value = "/countries",method = RequestMethod.GET)
+    @GetMapping(value = "/countries")
     public List<Country> country(){
         return countryService.getCountry();
     }
