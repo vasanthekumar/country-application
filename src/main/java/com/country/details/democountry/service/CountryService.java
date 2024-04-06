67d302078caf142aef06ae077a7c2eb71fba9487
Vasanth <vasanth.fine@yahoo.com>
2024-04-06 08:14:16 +0300

    Handled load balance.

--- src/main/java/com/country/details/democountry/service/CountryService.java
+++ src/main/java/com/country/details/democountry/service/CountryService.java
@@ -3,6 +3,7 @@ package com.country.details.democountry.service;
 import com.country.details.democountry.modal.Country;
 import com.country.details.democountry.repository.CountryRepository;
 import com.country.details.democountry.util.Client;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,12 @@ public class CountryService {
         return countryRepository.findAll();
     }
 
+    @CircuitBreaker(name = "country", fallbackMethod = "getCountryInfoByNamefallback")
     public String getCountryInfoByName(String countryName){
         return client.webClient().get().uri("/into/{name}",countryName).retrieve().bodyToMono(String.class).block();
+    }
+
+    public String getCountryInfoByNamefallback(Exception exception){
+        return new String();
     }
 }
