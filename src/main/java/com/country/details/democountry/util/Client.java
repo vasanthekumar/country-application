42b3f8226d7ef16ff07210de494ede81128508d5
Vasanth <vasanth.fine@yahoo.com>
2024-04-06 22:34:00 +0300

    Populate initial data.

--- src/main/java/com/country/details/democountry/util/Client.java
+++ src/main/java/com/country/details/democountry/util/Client.java
@@ -1,8 +1,10 @@
 package com.country.details.democountry.util;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class Client {
 
     @Bean
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO `country_db`.`country` (`country_code`, `name`) VALUES ('F1', 'Finland');
-INSERT INTO `country_db`.`country` (`country_code`, `name`) VALUES ('S1', 'Sweden');
+INSERT INTO country_db.country (country_code, name) VALUES ('F1', 'Finland');
+INSERT INTO country_db.country (country_code, name) VALUES ('S1', 'Sweden');
