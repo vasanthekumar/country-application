41f0fb913e1940e435c6b324bb4bf130dc2898b0
Vasanth <vasanth.fine@yahoo.com>
2024-04-07 17:00:29 +0300

    Added comments to code.

--- src/main/java/com/country/details/democountry/repository/CountryRepository.java
+++ src/main/java/com/country/details/democountry/repository/CountryRepository.java
@@ -4,7 +4,14 @@ import com.country.details.democountry.modal.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
+ * Repository interface for managing entities of type Country.
+ *
+ * This repository provides CRUD (Create,Read,Update,Delete) operations for entities of type Country.
+ *
  * @author kvasanthakumar
+ * @version 0.0.1
+ * Date: April 4,2024
  */
-public interface CountryRepository extends JpaRepository<Country,Long> {
+public interface CountryRepository extends JpaRepository<Country,Integer> {
+    //TODO
 }
--- src/main/java/com/country/details/democountry/service/CountryService.java
+++ src/main/java/com/country/details/democountry/service/CountryService.java
@@ -11,18 +11,33 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+/**
+ * Service class for managing entities of the type Country.
+ *
+ * This service provides business logic operation for managing entities of type Country.
+ * It interacts with the CountryRepository to perform CRUD (Create,Read,Update,Delete) operations.
+ *
+ * @author kvasanthakumar
+ * @version 0.0.1
+ * Date: April 4,2024
+ */
 @Service
 public class CountryService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
 
+    /**
+     * Properties injection to autowire CountryRepository dependency.
+     */
     @Autowired
     CountryRepository countryRepository;
 
+    /**
+     * Properties injection to autowire LoadBalancerClient dependency.
+     */
     @Autowired
     LoadBalancerClient loadBalancerClient;
 
@@ -33,6 +48,11 @@ public class CountryService {
         return WebClient.create(baseUrl);
     }
 
+    /**
+     * Retrives all countries.
+     *
+     * @return List of retrived countries, or empty list if not.
+     */
     public List<Country> getCountry() {
         return countryRepository.findAll();
     }
